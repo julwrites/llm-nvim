9942f739e0433fa460541aabc0ae95c4812f40f3
julwrites <mail@tehj.io>
2025-07-27 23:55:48 +0800

    Reviewed code, tests look like the wrong methodology to begin with. Cleaning and will restart

--- test/minimal_init.lua
+++
@@ -1,9 +0,0 @@
-vim.loader.enable()
-vim.opt.rtp:prepend(vim.fn.getcwd())
-
--- Set up package.path to include the lua directory
-package.path = package.path .. ";" .. vim.fn.getcwd() .. "/lua/?.lua;" .. vim.fn.getcwd() .. "/lua/?/init.lua"
-
--- Debugging: Print the current working directory and final package.path
-vim.notify("Current working directory: " .. vim.fn.getcwd(), vim.log.levels.INFO)
-vim.notify("Final package.path: " .. package.path, vim.log.levels.INFO)
